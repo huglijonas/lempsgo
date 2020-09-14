8ea06998d34954b06ad8b829768e1a71d6a2a99f
Jonas Hügli <huglijonas@gmail.com>
2020-09-14 20:38:16 +0200

    Fixes

--- deploy.sh
+++ deploy.sh
@@ -37,46 +37,421 @@ LIGHTPURPLE='\033[1;35m'
 LIGHTCYAN='\033[1;36m'
 WHITE='\033[1;37m'
 
+# --------------------------------------------------------------------
+# Install function
+# ----------------------------------
+function install() 
+{
+    PACKAGE=$1
+    LOG="src/log/"
+    case $PACKAGE in
+        [nginx]* ) LOG+="nginx.log";;
+        [php7.3]* ) LOG+="php.log";;
+        [mariadb]* ) LOG+="mariadb.log";;
+    esac
+    
+    printf $GREEN;
+    echo "[${PACKAGE^^}] Installation in progress"; 
+    printf $WHITE
+    # Loading bar
+    while true;do echo -n .;sleep 1;done &
+        # Installation (Redirection to nginx.log)
+        apt install $PACKAGE -y --no-install-recommends apt-utils &> $LOG
+        kill $!; trap 'kill $!' SIGTERM
+    printf $NOCOLOR
+    if [ !dpkg -s $PACKAGE &> /dev/null ]
+    then
+        printf $RED
+        echo "[${PACKAGE^^}] An error occured! Please see the mariadb log file in $LOG"
+        printf $NOCOLOR
+        exit
+    fi
+    printf $GREEN
+    echo "[${PACKAGE^^}] Successfully installed!"
+    printf $NOCOLOR
+    echo
+}
+
 # ----------------------------------
 # Uninstall function
 # ----------------------------------
-function uninstall() {
+function uninstall()
+{
     PACKAGE=$1
     LOG="src/log/"
-    return=true
+    result=true
 
     case $PACKAGE in
-        [nginx]*) LOG+="nginx.log";;
-        [php7.3]*) LOG+="php.log";;
-        [mariadb]*) LOG+="mariadb.log";;
+        [nginx]* ) LOG+="nginx.log";;
+        [php7.3]* ) LOG+="php.log";;
+        [mariadb]* ) LOG+="mariadb.log";;
     esac
     
     dpkg -s $PACKAGE &> /dev/null
-    if [ $? == 0 ] then
-    # Loop to ask if the user want to reinstall mariadb
-    while true; do
-        read -p "[${PACKAGE^^}] seems to be already installed. Would you reinstall the package? (y/n)" yn
-        case $yn in
-            [Yy]* ) printf $GREEN; echo "[${PACKAGE^^}] Uninstall in progress"; printf $WHITE;
-            # Loading bar
-            while true;do echo -n .;sleep 1;done &
-                # Erase the nginx.log file
-                > src/log/mariadb.log
-                # Remove & purge (Redirection to mariadb.log)
-                apt remove $PACKAGE -y --no-install-recommends apt-utils &> $LOG
-                apt purge $PACKAGE -y --no-install-recommends apt-utils &> $LOG
-                apt autoremove -y --no-install-recommends apt-utils &> $LOG
-                kill $!; trap 'kill $!' SIGTERM
-            printf $GREEN
-            echo "[${PACKAGE^^}] Successfully uninstalled!"
-            printf $NOCOLOR
-            break;;
-            [Nn]* ) printf $RED; echo "[${PACKAGE^^}] Deployment canceled"; printf $NOCOLOR; local return=false; break;;
-            * ) printf $RED; echo "Please answer yes or no."; printf $NOCOLOR;;
-        esac
-    done
+    if [ $? == 0 ] 
+    then
+        # Loop to ask if the user want to reinstall mariadb
+        while true; do
+            read -p "[${PACKAGE^^}] seems to be already installed. Would you reinstall the package? (y/n)" yn
+            case $yn in
+                [Yy]* ) printf $GREEN; echo -e "[${PACKAGE^^}] Uninstall in progress"; printf $WHITE;
+                # Loading bar
+                while true;do echo -n .;sleep 1;done &
+                    # Erase the nginx.log file
+                    > $LOG
+                    # Remove & purge (Redirection to mariadb.log)
+                    apt remove $PACKAGE -y --no-install-recommends apt-utils &> $LOG
+                    apt purge $PACKAGE -y --no-install-recommends apt-utils &> $LOG
+                    apt autoremove -y --no-install-recommends apt-utils &> $LOG
+                    kill $!; trap 'kill $!' SIGTERM
+                printf $GREEN
+                echo -e "[${PACKAGE^^}] Successfully uninstalled!"
+                printf $NOCOLOR
+                break;;
+                [Nn]* ) printf $RED; echo "[${PACKAGE^^}] Deployment canceled"; printf $NOCOLOR; result=false; break;;
+                * ) printf $RED; echo "Please answer yes or no."; printf $NOCOLOR;;
+            esac
+        done
+    fi
+    echo
+    local $result
 }
 
+# ----------------------------------
+# SOCK
+# ----------------------------------
+function change_sock()
+{
+    SOCK=$(grep -c "/var/run/php/php7.3-fpm.sock" /etc/php/7.3/fpm/pool.d/www.conf)
+    if [[ $SOCK == 0 ]]
+    then
+        sed -i 's/\/run\/php\/php7.3-fpm.sock/\/var\/run\/php\/php7.3-fpm.sock/g' /etc/php/7.3/fpm/pool.d/www.conf
+        printf $BLUE
+        echo "[PHP7.3-FPM] Sock path changed in /etc/php/7.3/fpm/pool.d/www.conf"
+        printf $NOCOLOR
+    fi
+    sed -i 's/; cgi.fix_pathinfo=1/cgi.fix_pathinfo=0/g' /etc/php/7.3/fpm/php.ini
+    printf $BLUE
+    echo "[PHP7.3-FPM] Pathinfo set to 0 in /etc/php/7.3/fpm/php.ini"
+    printf $NOCOLOR
+}
+
+# ----------------------------------
+# MARIADB INIT
+# ----------------------------------
+function mariadb_init()
+{
+    service mysql start > /dev/null
+    
+    #
+    # ROOT PWD
+    while true; do
+        read -p "Change root password? (y/n)" yn
+        case $yn in
+            [Yy]*) DB_PWD_CHANGE=true; break;;
+            [Nn]*) DB_PWD_CHANGE=false; break;;
+            *) printf $RED; echo "Please answer yes or no."; printf $NOCOLOR;;
+        esac
+    done
+    if [ $DB_PWD_CHANGE == true ]
+    then
+        while true; do
+            read -s -p "Enter the new password: " pwd
+            echo
+            read -s -p "Please confirm the new password: " confirmPwd
+            echo
+            if [ $pwd == $confirmPwd ]
+            then
+                mysql -e "UPDATE mysql.user SET Password=PASSWORD('${pwd}') WHERE User='root';"
+                if [ $? == 0 ]
+                then
+                    printf $GREEN; echo "Password changed for root!"; printf $NOCOLOR;
+                    echo
+                    break;
+                else
+                    printf $RED; echo "An error occured! Exiting..."; printf $NOCOLOR; exit;
+                fi
+            else
+                printf $RED; echo "The passwords do not match! Try again."; printf $NOCOLOR
+            fi
+        done
+    fi
+    
+    #
+    # ANONYMOUS USERS
+    while true; do
+        read -p "Remove anonymous users? (y/n)" yn
+        case $yn in
+            [Yy]*) DB_RM_ANONYM=true; break;;
+            [Nn]*) DB_RM_ANONYM=false; break;;
+            *) printf $RED; echo "Please answer yes or no."; printf $NOCOLOR;;
+        esac
+    done
+    if [ $DB_RM_ANONYM == true ]
+    then
+        mysql -e "DELETE FROM mysql.global_priv WHERE User='';"
+        if [ $0 == 0 ]
+        then
+            printf $GREEN; echo "Anonymous users removed!"; printf $NOCOLOR;
+        else
+            printf $RED; echo "An error occured!"; printf $NOCOLOR;
+        fi
+        echo
+    fi
+    
+    #
+    # DISALLOW REMOTE ROOT LOGIN
+    while true; do
+        read -p "Disallow root login remotely? (y/n)" yn
+        case $yn in
+            [Yy]*) DB_DIS_ROOT=true; break;;
+            [Nn]*) DB_DIS_ROOT=false; break;;
+            *) printf $RED; echo "Please answer yes or no."; printf $NOCOLOR;;
+        esac
+    done
+    if [ $DB_DIS_ROOT == true ]
+    then
+        mysql -e "DELETE FROM mysql.global_priv WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1');"
+        if [ $? == 0 ]
+        then
+            printf $GREEN; echo "Remote root login disabled!"; printf $NOCOLOR;
+        else
+            printf $RED; echo "An error occured!"; printf $NOCOLOR;
+        fi
+        echo
+    fi
+    
+    #
+    # REMOVE TEST DATABASE
+    while true; do
+        read -p "Remove test database and access to it? (y/n)" yn
+        case $yn in
+            [Yy]*) DB_RM_TST=true; break;;
+            [Nn]*) DB_RM_TST=false; break;;
+            *) printf $RED; echo "Please answer yes or no."; printf $NOCOLOR
+        esac
+    done
+    if [ $DB_RM_TST == true ]
+    then
+        mysql -e "DROP DATABASE test;"
+        if [ $? == 0 ]
+        then
+            printf $GREEN; echo "Test database removed!"; printf $NOCOLOR;
+        else
+            printf $RED; echo "An error occured!"; printf $NOCOLOR;
+        fi
+        echo
+    fi
+    
+    #
+    # INSTALL BLACKHOLE
+    while true; do
+        read -p "Install BLACKHOLE engine? (y/n)" yn
+        case $yn in
+            [Yy]*) DB_BLACKHOLE=true; break;;
+            [Nn]*) DB_BLACKHOLE=false; break;;
+            *) printf $RED; echo "Please answer yes or no."; printf $NOCOLOR;;
+        esac
+    done
+    if [ $DB_BLACKHOLE == true ]
+    then
+        mysql -e "INSTALL SONAME 'ha_blackhole';"
+        if [ $? == 0 ]
+        then
+            printf $GREEN; echo "BLACKHOLE Engine installed!"; printf $NOCOLOR;
+        else
+            printf $RED; echo "An error occured!"; printf $NOCOLOR;
+        fi
+        echo
+    fi
+    
+    mysql -e "FLUSH PRIVILEGES;"
+    if [ $? == 0 ]
+    then
+        printf $GREEN; echo "MySQL was securely installed"; printf $NOCOLOR;
+    else
+        printf $RED; echo "Critical error... please create an issue on github!"; printf $NOCOLOR; exit;
+    fi
+    service mysql start > /dev/null
+}
+
+# ----------------------------------
+# TEMPLATE
+# ----------------------------------
+function createTemplate()
+{
+    result = false
+    while true; do
+        printf $WHITE
+        read -p "[TEMPLATE] Do you want to initialize a fake website to see how NGINX works? (y/n)" yn
+        case $yn in
+            [Yy]*) TEMPLATE=true; break;;
+            [Nn]*) TEMPLATE=false; break;;
+            *) printf $RED; echo "Please answer yes or no."; printf $NOCOLOR;;
+        esac
+    done
+    if [ $TEMPLATE == true ] 
+    then
+        result=true
+        if [[ ! -d "/var/www/html" ]]
+        then
+            mkdir /var/www/html
+        fi
+        mkdir /var/www/html/lempsgo/; cp src/files/index.php /var/www/html/lempsgo/index.php; cp src/files/info.php /var/www/html/lempsgo/info.php;
+        chown -R www-data:www-data /var/www/html/lempsgo
+
+        cp src/files/lempsgo /etc/nginx/sites-available/lempsgo; ln -s /etc/nginx/sites-available/lempsgo /etc/nginx/sites-enabled/
+
+        nginx -t
+        if [[ $? -ne 0 ]]
+        then
+            printf $RED
+            echo "An error occured!"
+            printf $NOCOLOR
+            exit
+        fi
+
+        if [ command -v systemctl &> /dev/null ]
+        then
+            systemctl restart nginx
+            systemctl start php7.3-fpm && systemctl restart php7.3-fpm
+            systemctl restart mysql
+        else
+            service nginx restart
+            service php7.3-fpm start && service php7.3-fpm restart
+            service mysql restart
+        fi
+        
+        echo
+        printf $GREEN
+        echo "LEMP'S GO! Go to the server URL like this: http://lempsgo.localhost/"
+        printf $NOCOLOR
+    fi
+    
+    local $result
+}
+
+# ----------------------------------
+# CERTIFICATE
+# ----------------------------------
+function generateCertificate()
+{
+    printf $WHITE
+    while true; do
+        read -p "[CERTIFICATE] Do you want to generate a certificate for the fake website to see how NGINX and HTTPS works? (y/n)" yn
+        case $yn in
+            [Yy]*) CERT=true; break;;
+            [Nn]*) CERT=false; break;;
+            *) printf $RED; echo "Please answer yes or no."; printf $NOCOLOR;;
+        esac
+    done
+    
+    # Country
+    CountryRegex='^[A-Z]{2}$'
+    while true; do
+        printf $WHITE
+        read -p "[CERTIFICATE] Country name (2 letter code): " Country
+        if [[ ${Country^^} =~ $CountryRegex ]]
+        then
+            break
+        else
+            printf $RED; echo "Please enter a correct country."; printf $NOCOLOR;
+        fi
+    done
+    
+    # State
+    StateRegex='^([a-zA-Z]+|[a-zA-Z]+\s[a-zA-Z]+)$'
+    while true; do
+        printf $WHITE
+        read -p "[CERTIFICATE] State or province name (full name): " State
+        if [[ ${State} =~ $StateRegex ]]
+        then
+            break
+        else
+            printf $RED; echo "Please enter a correct state."; printf $NOCOLOR;
+        fi
+    done
+    
+    # City
+    CityRegex='^([a-zA-Z]+|[a-zA-Z]+\s[a-zA-Z]+)$'
+    while true; do
+        printf $WHITE
+        read -p "[CERTIFICATE] Locality name (eg, city): " City
+        if [[ $City =~ $City ]]
+        then
+            break
+        else
+            printf $RED; echo "Please enter a correct city."; printf $NOCOLOR;
+        fi
+    done
+    
+    # Organization
+    OrgRegex='|(^([a-zA-Z]+|[a-zA-Z]+\s[a-zA-Z]+)$)'
+    while true; do
+        printf $WHITE
+        read -p "[CERTIFICATE] Organization name (eg, company): " Organization
+        if [[ $Organization =~ $OrgRegex ]]
+        then
+            if [[ -z $Organisation ]]
+            then
+                Organization="None"
+            fi
+            break
+        else
+            printf $RED; echo "Please enter a correct organization."; printf $NOCOLOR;
+        fi
+    done
+    
+    # Website
+    WebRegex='^(http:\/\/www\.|https:\/\/www\.|http:\/\/|https:\/\/)?[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?$'
+    while true; do
+        printf $WHITE
+        read -p "[CERTIFICATE] Website URL (eg, FQDN): " Website
+        if [[ $Website =~ $WebRegex ]]
+        then
+            break
+        else
+            printf $RED; echo "Please enter an correct URL."; printf $NOCOLOR;
+        fi
+    done
+    
+    openssl req -new -subj "/C=${Country^^}/ST=$State/L=$City/O=$Organization/CN=$Website" -x509 -nodes -days 365 -newkey rsa:4096 -keyout /etc/ssl/private/lempsgo.key -out /etc/ssl/certs/lempsgo.crt
+    
+    if [ $? -ne 0 ]
+    then
+        printf $RED; echo "A fatal error occured..."; printf $NOCOLOR
+    fi
+    
+    echo
+    printf $PURPLE
+    printf "Generate a Diffie-Hellman key"; sleep .2; printf "."; sleep .2; printf "."; sleep .2; printf ".";
+    echo
+    printf $WHITE
+    openssl dhparam -dsaparam -out /etc/nginx/dhp_key.pem 4096
+    
+    cp src/files/certificate.conf /etc/nginx/snippets/certificate.conf
+    cp src/files/ssl-params.conf /etc/nginx/snippets/ssl-params.conf
+    cat src/files/lempsgo-ssl > /etc/nginx/sites-available/lempsgo
+    
+    printf $GREEN
+    echo "LEMP'S GO! Go to the server URL like this: https://lempsgo.localhost/"
+    printf $NOCOLOR
+}
+
+# ----------------------------------
+# PERMISSIONS
+# ----------------------------------
+if [ "$EUID" -ne 0 ]
+  then
+    printf $RED
+    echo -e "Please run as root or execute with the sudo command"
+    printf $NOCOLOR
+  exit
+fi
+
+# --------------------------------------------------------------------
+# BEGIN SCRIPT
+# ----------------------------------
 printf $WHITE
 echo -e " +--------------------------------------------+"
 sleep .2
@@ -107,96 +482,92 @@ echo -e " +--------------------------------------------+"
 printf $NOCOLOR
 
 # ----------------------------------
-# Verify permission
+# UNINSTALLATIONS
 # ----------------------------------
-if [ "$EUID" -ne 0 ]
-  then
-    printf $RED
-    echo -e "Please run as root or execute with the sudo command"
+uninstall nginx result
+NGINX=$result
+uninstall mariadb-server result
+MARIADB=$result
+uninstall php7.3-fpm result
+PHPFPM=$result
+uninstall php7.3-mysql result
+PHPMYSQL=$result
+
+# ----------------------------------
+# INSTALLATIONS
+# ----------------------------------
+if [ $NGINX == true ]
+then
+    printf $GREEN
+    printf "Initialization of the NGINX step"; sleep .2; printf "."; sleep .2; printf "."; sleep .2; printf "."
     printf $NOCOLOR
-  exit
+    echo
+    install nginx
+fi
+if [ $MARIADB == true ]
+then
+    printf $ORANGE
+    printf "Initialization of the MARIADB step"; sleep .2; printf "."; sleep .2; printf "."; sleep .2; printf "."
+    printf $NOCOLOR
+    echo
+    install mariadb-server
+fi
+if [ $PHPFPM == true ]
+then
+    printf $BLUE
+    printf "Initialization of the PHP-FPM step"; sleep .2; printf "."; sleep .2; printf "."; sleep .2; printf "."
+    printf $NOCOLOR
+    echo
+    install php7.3-fpm
+fi
+if [ $PHPMYSQL == true ]
+then
+    printf $BLUE
+    printf "Initialization of the PHP-MYSQL step"; sleep .2; printf "."; sleep .2; printf "."; sleep .2; printf "."
+    printf $NOCOLOR
+    echo
+    install php7.3-mysql
 fi
 
-while true; do
-    read -p "Are you sure to perform the LEMP installation? (y/n)" yn
-    case $yn in
-        [Yy]* ) break;;
-        [Nn]* ) exit;;
-        * ) printf $RED; echo "Please answer yes or no."; printf $NOCOLOR;;
-    esac
-done
-
 # ----------------------------------
-# NGINX
-# ----------------------------------
-printf $GREEN
-printf "Initialization of the NGINX step"; sleep .2; printf "."; sleep .2; printf "."; sleep .2; printf "."
-printf $NOCOLOR
-echo
-bash src/scripts/nginx.sh
-
-# ----------------------------------
-# PHP
-# ----------------------------------
-printf $ORANGE
-printf "Initialization of the MARIADB step"; sleep .2; printf "."; sleep .2; printf "."; sleep .2; printf "."
-printf $NOCOLOR
-echo
-bash src/scripts/mariadb.sh
-
-# ----------------------------------
-# PHP
+# SOCK
 # ----------------------------------
 printf $BLUE
-printf "Initialization of the PHP step"; sleep .2; printf "."; sleep .2; printf "."; sleep .2; printf "."
+printf "FPM SOCK PATH"; sleep .2; printf "."; sleep .2; printf "."; sleep .2; printf "."
 printf $NOCOLOR
 echo
-bash src/scripts/php.sh
-
-# ----------------------------------
-# HTML
-# ----------------------------------
-while true; do
-    read -p "Do you want to initialize a fake website to see how NGINX works? (y/n)" yn
-    case $yn in
-        [Yy]* ) WEBSITE=true; break;;
-        [Nn]* ) WEBSITE=false; break;;
-        * ) printf $RED; echo "Please answer yes or no."; printf $NOCOLOR;;
-    esac
-done
-if [ $WEBSITE == true ]
-then
-    if [[ ! -d "/var/www/html" ]]
-    then
-        mkdir /var/www/html
-    fi
-    mkdir /var/www/html/lempsgo/; cp src/files/index.php /var/www/html/lempsgo/index.php; cp src/files/info.php /var/www/html/lempsgo/info.php;
-    chown -R www-data:www-data /var/www/html/lempsgo
-    
-    cp src/files/lempsgo /etc/nginx/sites-available/lempsgo; ln -s /etc/nginx/sites-available/lempsgo /etc/nginx/sites-enabled/
-    
-    nginx -t
-    if [[ $! -ne 0 ]]
-    then
-        printf $RED
-        echo "An error occured!"
-        printf $NOCOLOR
-        exit
-    fi
-    
-    if [ command -v systemctl &> /dev/null ]
-    then
-        systemctl restart nginx
-        systemctl start php7.3-fpm && systemctl restart php7.3-fpm
-        systemctl restart mysql
-    else
-        service nginx restart
-        service php7.3-fpm start && service php7.3-fpm restart
-        service mysql restart
-    fi
-fi
-
+change_sock
 echo
+
+# ----------------------------------
+# MARIADB
+# ----------------------------------
+printf $ORANGE
+printf "Initialization of Mariadb"; sleep .2; printf "."; sleep .2; printf "."; sleep .2; printf "."
+printf $NOCOLOR
+echo
+mariadb_init
+echo
+
+# ----------------------------------
+# TEMPLATE
+# ----------------------------------
 printf $GREEN
-echo "LEMP'S GO! Go to the server URL like this: http://lempsgo.localhost/"
+printf "Initialization of template"; sleep .2; printf "."; sleep .2; printf "."; sleep .2; printf "."
 printf $NOCOLOR
+echo
+createTemplate templateCreation
+echo
+
+# ----------------------------------
+# CERTIFICATE
+# ----------------------------------
+if [[ $templateCreation == true ]]
+then
+    printf $PURPLE
+    printf "Initialization of certificate"; sleep .2; printf "."; sleep .2; printf "."; sleep .2; printf "."
+    printf $NOCOLOR
+    echo
+    generateCertificate
+fi
+# --------------------------------------------------------------------
